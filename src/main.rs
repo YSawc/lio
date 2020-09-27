735c37626a733a34c5388f69b81aff24ac0eb3f2
YSawc <niceage6690@gmail.com>
2020-09-28 08:34:30 +0900

    Implement loop input.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,37 @@ use rr::code_gen::gen_x86::*;
 use rr::node::node::*;
 use rr::token::token::*;
 use std::env;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let arg1 = &args[1];
 
-    println!("INPUT: {}", arg1);
-    let t = Token::tokenize(arg1).unwrap();
-    println!("{:?}", t);
-    let _nst = NodeSt::parser(t);
-    println!("{:?}", _nst);
-    let _nst = gen(_nst);
+    if arg1 == "repl" {
+        loop {
+            print!("> ");
+            std::io::stdout().flush().unwrap();
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).unwrap();
+            let s = s.trim();
+
+            if s == "q" {
+                break;
+            }
+
+            let t = Token::tokenize(&s).unwrap();
+            // println!("{:?}", t);
+            let _nst = NodeSt::parser(t);
+            println!("{:?}", _nst);
+            let _nst = gen(_nst);
+        }
+    } else {
+        println!("INPUT: {}", arg1);
+        let t = Token::tokenize(arg1).unwrap();
+        println!("{:?}", t);
+        let _nst = NodeSt::parser(t);
+        // println!("{:?}", _nst.c);
+        println!("{:?}", _nst);
+        let _nst = gen(_nst);
+    }
 }
